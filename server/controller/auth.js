2856c8b0ca204f57ea2f7c26e8668452d4fb2a5d
Rosy <prosy56@gmail.com>
2021-04-20 19:14:50 +0530

    second commit

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -71,7 +71,7 @@ class Auth {
                 email,
                 password,
                 // ========= Here role 1 for admin signup role 0 for customer signup =========
-                role: 1,
+                role: 0,
               });
               newUser
                 .save()
